445ee5d9f49a47dcff129239a6f198b143865a92
monimag1262 <monimag@amazon.com>
2026-03-05 11:40:51 -0700

    Update version number to 3.5.26

--- urgent-site-alert.user.js
+++ urgent-site-alert.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Amazon Relay Urgent Site Alert
 // @namespace    http://tampermonkey.net/
-// @version      1.0.1
+// @version      3.5.26
 // @description  In-place notification for urgency: 1P Threshold - Yard Capacity
 // @author       monimag
 // @match        https://aap-na.corp.amazon.com/*
